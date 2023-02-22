31f64eda7b718c9ede4125a78d38ebda50419d58
noaqq <maximbaymetov2002@icloud.com>
2023-02-22 19:57:22 +0300

    fix static files

--- food_delivery/food_delivery/settings.py
+++ food_delivery/food_delivery/settings.py
@@ -119,11 +119,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-#static не работает, потому что я дебил
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#     'food_delivery/delivery/static/',]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
