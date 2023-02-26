6cbbb6ecf0a54b67a7839248cc49a19b489304ab
noaqq <maximbaymetov2002@icloud.com>
2023-02-26 18:56:44 +0300

    register

--- delivery/views.py
+++ delivery/views.py
@@ -1,9 +1,11 @@
-# from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth import login
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-# from .forms import NewUserForm
+from delivery.forms import NewUserForm
+
+from .forms import NewUserForm
 
 
 def start(request):
@@ -26,14 +28,14 @@ def delivery(request):
     return render(request, "delivery/delivery.html")
 
 
-# def register_user(request):
-# 	if request.method == "POST":
-# 		form = NewUserForm(request.POST)
-# 		if form.is_valid():
-# 			user = form.save()
-# 			login(request, user)
-# 			messages.success(request, "Registration successful." )
-# 			return redirect("delivery/start.html")
-# 		messages.error(request, "Unsuccessful registration. Invalid information.")
-# 	form = NewUserForm()
-# 	return render (request=request, template_name="delivery/register.html", context={"register_form":form})
+def register_user(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("delivery/start.html")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="delivery/register.html", context={"register_form":form})
