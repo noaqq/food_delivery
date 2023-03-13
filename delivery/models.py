6c57356626ddac99aeadbb3b89ef1de75500fdb5
noaqq <maximbaymetov2002@icloud.com>
2023-03-13 17:20:57 +0300

    create model

--- delivery/models.py
+++ delivery/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = (
+    ("Молочная продукция", "Молочная продукция"),
+    ("Мясная гастрономия", "Мясная гастрономия"),
+    ("Выпечка", "Выпечка"),
+    ("Бакалея", "Бакалея"),
+    ("Полуфабрикаты", "Полуфабрикаты"),
+    ("Напитки", "Напитки"),
+    ("Снеки", "Снеки"),
+)
+
+
+class catalog(models.Model):
+    name = models.CharField("Название товара", max_length=100)
+    category = models.CharField("Категория", choices=CATEGORY_CHOICES, max_length=20)
+    price = models.FloatField()
+    description = models.CharField("Описание товара", max_length=150)
+    expiration_date = models.CharField("Срок годности", max_length=30)
+    image = models.ImageField(blank=True, upload_to="static/assets/food")
+
+    def __str__(self):
+        return self.name
