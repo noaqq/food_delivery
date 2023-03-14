9a6ff9625031efd40bd90fe2a4e4981c1fc630ea
noaqq <maximbaymetov2002@icloud.com>
2023-03-14 20:56:18 +0300

    fix class catalog

--- delivery/models.py
+++ delivery/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 
 CATEGORY_CHOICES = (
-    ("Молочная продукция", "Молочная продукция"),
-    ("Мясная гастрономия", "Мясная гастрономия"),
+    ("Молоко, яйца, масло", "Молоко, яйца, масло"),
+    ("Мясо и птица", "Мясо и птица"),
     ("Выпечка", "Выпечка"),
     ("Бакалея", "Бакалея"),
     ("Полуфабрикаты", "Полуфабрикаты"),
     ("Напитки", "Напитки"),
-    ("Снеки", "Снеки"),
+    ("Чипсы, орехи, сухофрукты", "Чипсы, орехи, сухофрукты"),
 )
 
 
 class catalog(models.Model):
     name = models.CharField("Название товара", max_length=100)
-    category = models.CharField("Категория", choices=CATEGORY_CHOICES, max_length=20)
-    price = models.FloatField()
+    category = models.CharField("Категория", choices=CATEGORY_CHOICES, max_length=50)
+    price = models.IntegerField()
     description = models.CharField("Описание товара", max_length=150)
-    expiration_date = models.CharField("Срок годности", max_length=30)
+    expiration_date = models.CharField("Срок годности", max_length=50)
     image = models.ImageField(blank=True, upload_to="delivery/static/assets/food")
 
     def __str__(self):
